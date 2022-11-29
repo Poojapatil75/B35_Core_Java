030f13066bb5c40f015c81406558489df8ad719d
Admin <Admin@DESKTOP-V69TB7R>
2022-11-29 11:10:31 +0530

    First commit

---
+++ Day-7_abstractionAndInstnaceOf/src/org/tnsindia/abstractdemo/ATMMachineChild.java
@@ -0,0 +1,11 @@
+package org.tnsindia.abstractdemo;
+
+public class ATMMachineChild extends ATMMachine  {
+void withdraw() {
+		
+		int balance=50000;
+		System.out.println("Withdrawing amount is: "+balance);
+		
+	}
+
+}
---
+++ Day-7_abstractionAndInstnaceOf/src/org/tnsindia/abstractdemo/ATMMachineExecutor.java
@@ -0,0 +1,16 @@
+package org.tnsindia.abstractdemo;
+
+public class ATMMachineExecutor {
+
+	public static void main(String[] args) {
+		ATMMachine a=new ATMMachineChild();
+		//ATMMachineChild a=new ATMMachineChild();
+		a.withdraw();
+		a.display();
+
+	}
+
+
+	}
+
+
