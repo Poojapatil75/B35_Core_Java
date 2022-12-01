c0e7676c8b38c5d73bb85791a5d4a365b643566f
Admin <Admin@DESKTOP-V69TB7R>
2022-12-01 16:10:05 +0530

    First Commit

---
+++ Day-9_thisAndStaticKeyword/src/org/tnsindia/staticdemo/StaticExecutor.java
@@ -0,0 +1,19 @@
+package org.tnsindia.staticdemo;
+
+public class StaticExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*StaticKeywordWithVariable s=new StaticKeywordWithVariable();
+		System.out.println(s.a);*/
+		System.out.println(StaticKeywordDemo.a);
+		StaticKeywordDemo.accept();
+	}
+
+}
+
+
+	
+
+
