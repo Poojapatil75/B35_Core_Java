131813b6aa17e714366b6f562377f736186acf5d
Admin <Admin@DESKTOP-V69TB7R>
2022-12-19 10:53:35 +0530

    First Commit

---
+++ Day18_JUnit5/src/com/capgemini/JunitDemo/RepeatedTestDemo.java
@@ -0,0 +1,17 @@
+package com.capgemini.JunitDemo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+class RepeatedTestDemo {
+
+	@RepeatedTest(3)
+	void test() {
+		
+		System.out.println("Welcome Dear Students");
+		
+	}
+
+}
